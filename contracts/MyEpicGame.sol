f91efd2037c8804dbadfe145836086f51ad562e6
Matteo Mervoyer <mmervoyer@yolaw.fr>
2021-12-01 15:06:37 +0100

    Changes to the contract

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -9,26 +9,25 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "./libraries/Base64.sol";
 
 contract MyEpicGame is ERC721 {
-
     struct CharacterAttr {
-        uint index;
+        uint256 index;
         string name;
         string imageURI;
-        uint hp;
-        uint maxhp;
-        uint attackDamage;
-        uint attackPower;
+        uint256 hp;
+        uint256 maxhp;
+        uint256 attackDamage;
+        uint256 attackPower;
     }
 
     struct Boss {
-        uint index;
+        uint256 index;
         string name;
         string imageURI;
-        uint hp;
-        uint maxhp;
-        uint attack;
-        uint ratioAD;
-        uint ratioAP;
+        uint256 hp;
+        uint256 maxhp;
+        uint256 attack;
+        uint256 ratioAD;
+        uint256 ratioAP;
     }
 
     using Counters for Counters.Counter;
@@ -39,70 +38,88 @@ contract MyEpicGame is ERC721 {
     Boss[] defaultBoss;
     Boss private actualBoss;
 
-    event NewBoss(uint bossId, string bossName);
-    event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
-    event AttackComplete(uint newBossHp, uint newPlayerHp);
+    event NewBoss(uint256 bossId, string bossName);
+    event CharacterNFTMinted(
+        address sender,
+        uint256 tokenId,
+        uint256 characterIndex
+    );
+    event AttackComplete(uint256 newBossHp, uint256 newPlayerHp);
 
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURI,
-        uint[] memory characterHP,
-        uint[] memory characterAD,
-        uint[] memory characterAP,
+        uint256[] memory characterHP,
+        uint256[] memory characterAD,
+        uint256[] memory characterAP,
         string[] memory bossNames,
         string[] memory bossImageURI,
-        uint[] memory bossHP,
-        uint[] memory bossAttack,
-        uint[] memory bossRatioAD,
-        uint[] memory bossRatioAP
-
-    ) ERC721 ("Witchers", "WTCHR"){
-        for (uint i = 0; i < bossNames.length; i++){
-            defaultBoss.push(Boss({
-                index: i,
-                name: bossNames[i],
-                imageURI: bossImageURI[i],
-                hp: bossHP[i],
-                maxhp: bossHP[i],
-                attack: bossAttack[i],
-                ratioAD: bossRatioAD[i],
-                ratioAP: bossRatioAP[i]
-            }));
+        uint256[] memory bossHP,
+        uint256[] memory bossAttack,
+        uint256[] memory bossRatioAD,
+        uint256[] memory bossRatioAP
+    ) ERC721("Witchers", "WTCHR") {
+        for (uint256 i = 0; i < bossNames.length; i++) {
+            defaultBoss.push(
+                Boss({
+                    index: i,
+                    name: bossNames[i],
+                    imageURI: bossImageURI[i],
+                    hp: bossHP[i],
+                    maxhp: bossHP[i],
+                    attack: bossAttack[i],
+                    ratioAD: bossRatioAD[i],
+                    ratioAP: bossRatioAP[i]
+                })
+            );
 
             Boss memory c = defaultBoss[i];
-            console.log("Done initializing boss %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+            console.log(
+                "Done initializing boss %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
         }
 
-        for (uint i = 0; i < characterNames.length ; i++){
-            defaultCharacters.push(CharacterAttr({
-                index: i,
-                name: characterNames[i],
-                imageURI: characterImageURI[i],
-                hp: characterHP[i],
-                maxhp: characterHP[i],
-                attackDamage: characterAD[i],
-                attackPower: characterAP[i]
-            }));
+        for (uint256 i = 0; i < characterNames.length; i++) {
+            defaultCharacters.push(
+                CharacterAttr({
+                    index: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURI[i],
+                    hp: characterHP[i],
+                    maxhp: characterHP[i],
+                    attackDamage: characterAD[i],
+                    attackPower: characterAP[i]
+                })
+            );
 
             CharacterAttr memory c = defaultCharacters[i];
-            console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
-
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
         }
         _tokenIds.increment();
         generateNewBoss();
     }
 
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
-    } 
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(abi.encodePacked(block.difficulty, block.timestamp))
+            );
+    }
 
-    function getBoss() public view returns (Boss memory){
+    function getBoss() public view returns (Boss memory) {
         return actualBoss;
     }
 
     function generateNewBoss() public {
-
-        uint bossId = random() % defaultBoss.length;
+        uint256 bossId = random() % defaultBoss.length;
         Boss memory newBoss = Boss({
             index: bossId,
             name: defaultBoss[bossId].name,
@@ -118,14 +135,17 @@ contract MyEpicGame is ERC721 {
         emit NewBoss(bossId, newBoss.name);
     }
 
-    function getAllDefaultCharacters() public view returns (CharacterAttr[] memory) {
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttr[] memory)
+    {
         return defaultCharacters;
     }
 
     function attackBoss() public {
-
         // If the boss is dead, bring a new one!
-        if (actualBoss.hp == 0){
+        if (actualBoss.hp == 0) {
             generateNewBoss();
         }
 
@@ -136,26 +156,51 @@ contract MyEpicGame is ERC721 {
         require(player.hp > 0, "The attacker is already dead !");
 
         //player attacks first!
-        uint playerOverallAttackVsBoss = (player.attackDamage * actualBoss.ratioAD) + (player.attackPower * actualBoss.ratioAP);
+        uint256 playerOverallAttackVsBoss = (player.attackDamage *
+            actualBoss.ratioAD) + (player.attackPower * actualBoss.ratioAP);
 
-        if (actualBoss.hp < playerOverallAttackVsBoss){
+        if (actualBoss.hp < playerOverallAttackVsBoss) {
             actualBoss.hp = 0;
         } else {
             actualBoss.hp -= playerOverallAttackVsBoss;
         }
 
         //boss attacks in return
-        if (player.hp < actualBoss.attack){
+        if (player.hp < actualBoss.attack) {
             player.hp = 0;
         } else {
             player.hp -= actualBoss.attack;
         }
 
-        console.log("After the combat, boss %s is at %d HP and player is at %d HP", actualBoss.name, actualBoss.hp, player.hp);
+        console.log(
+            "After the combat, boss %s is at %d HP and player is at %d HP",
+            actualBoss.name,
+            actualBoss.hp,
+            player.hp
+        );
         emit AttackComplete(actualBoss.hp, player.hp);
     }
 
-    function tokenURI(uint _tokenId) public view override returns (string memory){
+    function checkIfUserHasNFT() public view returns (CharacterAttr memory) {
+        // Get the tokenId of the user's character NFT
+        uint256 userNftTokenId = nftHolders[msg.sender];
+        // If the user has a tokenId in the map, return their character.
+        if (userNftTokenId > 0) {
+            return nftHolderAttr[userNftTokenId];
+        }
+        // Else, return an empty character.
+        else {
+            CharacterAttr memory emptyStruct;
+            return emptyStruct;
+        }
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
         CharacterAttr memory charAttributes = nftHolderAttr[_tokenId];
 
         string memory strHp = Strings.toString(charAttributes.hp);
@@ -167,11 +212,21 @@ contract MyEpicGame is ERC721 {
             bytes(
                 string(
                     abi.encodePacked(
-                        '{"name": "', charAttributes.name, ' #', Strings.toString(_tokenId),
-                        '", "description": "This NFT let people play the best NFT game ever", "image": "', charAttributes.imageURI,
-                         '", "attributes": [{"trait_type": "Health Points", "value": ', strHp,
-                         ', "max_value":', strMaxHp,'}, {"trait_type": "Attack Damage", "value": ', strAD,
-                         '}, {"trait_type": "Attack Power", "value": ', strAP, '}]}' 
+                        '{"name": "',
+                        charAttributes.name,
+                        " #",
+                        Strings.toString(_tokenId),
+                        '", "description": "This NFT let people play the best NFT game ever", "image": "',
+                        charAttributes.imageURI,
+                        '", "attributes": [{"trait_type": "Health Points", "value": ',
+                        strHp,
+                        ', "max_value":',
+                        strMaxHp,
+                        '}, {"trait_type": "Attack Damage", "value": ',
+                        strAD,
+                        '}, {"trait_type": "Attack Power", "value": ',
+                        strAP,
+                        "}]}"
                     )
                 )
             )
@@ -184,8 +239,8 @@ contract MyEpicGame is ERC721 {
         return output;
     }
 
-    function mintNFT(uint _characterIndex) external {
-        uint tokenId = _tokenIds.current();
+    function mintNFT(uint256 _characterIndex) external {
+        uint256 tokenId = _tokenIds.current();
 
         _safeMint(msg.sender, tokenId);
 
@@ -199,12 +254,15 @@ contract MyEpicGame is ERC721 {
             attackPower: defaultCharacters[_characterIndex].attackPower
         });
 
-        console.log("Minted NFT w/ tokenId %s and characterIndex %s", tokenId, _characterIndex);
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            tokenId,
+            _characterIndex
+        );
 
         nftHolders[msg.sender] = tokenId;
 
         _tokenIds.increment();
         emit CharacterNFTMinted(msg.sender, tokenId, _characterIndex);
     }
-
 }
